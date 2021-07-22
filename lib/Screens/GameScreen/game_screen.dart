6d89fe2326f91033a40e4b8fc1581bb3264d247a
Shihab uddin <Contact.shihab.247@gmail.com>
2021-07-22 16:23:26 +0600

    GameScreen work started

---
+++ lib/Screens/GameScreen/game_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class GameScreen extends StatefulWidget {
+  GameScreen({Key? key}) : super(key: key);
+
+  @override
+  _GameScreenState createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+
+  int score = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: Text(""),
+      ),
+      body: Column(),
+    );
+  }
+}
